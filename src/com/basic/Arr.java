9cf0745278456cbf7e7cdeeedfa7c4c72b194ca5
AISWARYARAJU <aiswaryaraju08@gmail.com>
2018-06-01 16:00:54 +0530

    PRINTING OF ARRAY ELEMENTS SEPARATELY

---
+++ src/com/basic/Arr.java
@@ -0,0 +1,25 @@
+package com.basic;
+
+public class Arr {
+	public static void main(String [] args)
+	{
+		int N=78945;
+		int t=N;
+	    int array []=new int[10];
+	    int i=0;
+	  
+	    while(t>0) 
+	    {
+	    	  int remainder= t%10;
+	    	  array[i]=remainder;
+	    	  i++;
+	    	  t=t/10;
+	    }
+	
+	    for( i=0;i<10;i++)
+		{
+			System.out.println(array[i]);
+		}
+	}
+
+}
