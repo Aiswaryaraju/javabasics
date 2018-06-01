471c4edc6da7bf5a748abb0d462c07ed4146d6e4
AISWARYARAJU <aiswaryaraju08@gmail.com>
2018-06-01 12:54:17 +0530

    Using of constructors through out the program

---
+++ src/com/basic/Car.java
@@ -0,0 +1,34 @@
+package com.basic;
+
+public class Car {
+	private String model;
+	int i=10;
+	public double speed=20;
+	public Car() 
+	{
+		System.out.println("empty cons");
+	}
+	public Car(int speed)
+	{
+		this();
+		this.speed=speed;
+		System.out.println("sinlge args");
+	}
+	public Car(int speed,String model)
+	{
+		this(speed);
+		this.speed=speed;
+		this.model=model;
+		System.out.println("double args");
+	}
+	public static void main(String args[])
+	{
+		//Car c1=new Car(100);
+		Car c2=new Car(100,"BMW");
+		System.out.println(c2.speed);
+		
+	
+		
+	}
+
+}
--- src/com/basic/Rectangle.java
+++ src/com/basic/Rectangle.java
@@ -15,14 +15,14 @@ public class Rectangle {
 		return length;
 	}
 	public void setLength(double length) {
-		if(length>0.0 && width<20.0)
+		if(length>0.0 && length<20.0)
 		this.length = length;
 	}
 	public double getWidth() {
 		return width;
 	}
 	public void setWidth(double width) {
-		if(length>0.0 && width <20.0)
+		if(width>0.0 && width <20.0)
 		this.width = width;
 	}
 	public double findArea()
