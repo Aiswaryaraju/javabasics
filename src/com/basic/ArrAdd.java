b3a4e9b4a65fb523cd973de7b92fad143a48a43e
AISWARYARAJU <aiswaryaraju08@gmail.com>
2018-06-02 09:32:07 +0530

    ADDITION IN ARRAY

---
+++ src/com/basic/ArrAdd.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+public class ArrAdd {
+	public static void main(String  args[]) {
+		for(int i=0;i<args.length;i++)
+		{
+			String a="1";
+		int b=Integer.parseInt(a);
+		double c=Double.parseDouble(a);
+		System.out.println(args[i]);
+		}
+		
+	}
+	
+}
