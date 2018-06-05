ac21bbd00bc337291117aee3b1af912eb11aba33
AISWARYARAJU <aiswaryaraju08@gmail.com>
2018-06-05 11:47:08 +0530

    SHAPE 1

---
+++ src/com/basic/Shape.java
@@ -0,0 +1,12 @@
+package com.basic;
+
+public class Shape {
+	static float length;
+	static double area;
+	static float height;
+	
+	public void diagram() {
+		System.out.println("SHAPES AND ITS AREA");
+	}
+
+}
