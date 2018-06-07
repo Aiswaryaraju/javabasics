35967c91e2e52a4d52383769dad8c2b6ed4ea75d
AISWARYARAJU <aiswaryaraju08@gmail.com>
2018-06-07 10:28:08 +0530

    java interface 4

---
+++ src/com/basic/Rent.java
@@ -0,0 +1,13 @@
+package com.basic;
+
+public class Rent {
+	public static void main(String[] args) {
+		
+	Tour cool=new Ooty();
+	Tour start2=new Star2();
+	System.out.println(start2 instanceof Star2 );
+	System.out.println(cool instanceof Cbe );
+
+}
+
+}
