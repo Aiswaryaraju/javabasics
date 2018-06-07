575aa8982df69212ba9156232d344d9c268ab0e3
AISWARYARAJU <aiswaryaraju08@gmail.com>
2018-06-07 15:34:51 +0530

    prg 1

--- src/com/basic/Tour.java
+++ src/com/basic/Tour.java
@@ -1,12 +1,15 @@
 package com.basic;
 
+import com.iet.org.Details;
+
 public interface Tour {
 	public void getAmount();
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) 
+	{
 		
-		
-		
+		Car c2=new Car(100,"BMW");
+		Student student1=new Student();
 		
 	}
-
 }
