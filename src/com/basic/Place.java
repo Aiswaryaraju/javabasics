d73de5b3740560f9ec89fcf6e3eebe92b3c02537
AISWARYARAJU <aiswaryaraju08@gmail.com>
2018-06-07 10:27:47 +0530

    java interface 3

---
+++ src/com/basic/Place.java
@@ -0,0 +1,42 @@
+package com.basic;
+
+public abstract class Place implements Tour {
+	
+
+}
+class Ooty extends Place {
+
+public void getAmount()
+{
+	System.out.println("PLACE VISITED OOTY");
+}
+
+	public static void main(String[] args) {
+		Ooty cool = new Ooty();
+		
+	}
+}
+class Trichy extends Place {
+
+public void getAmount()
+{
+	System.out.println("PLACE VISITED TRICHY");
+}
+
+	public static void main(String[] args) {
+		Trichy hot = new Trichy();
+		
+	}
+}
+class Cbe extends Place {
+
+public void getAmount()
+{
+	System.out.println("PLACE VISITED CBE");
+}
+
+	public static void main(String[] args) {
+		Cbe moderate = new Cbe();
+		
+	}
+}
