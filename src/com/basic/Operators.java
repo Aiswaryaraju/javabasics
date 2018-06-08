321327ce4feca8a2dd9a4a3be7b5c749c6840077
AISWARYARAJU <aiswaryaraju08@gmail.com>
2018-06-08 21:38:02 +0530

    USING OF Operators AND LOOPS

--- src/com/basic/Operators.java
+++ src/com/basic/Operators.java
@@ -1,7 +1,64 @@
 package com.basic;
 
-public class Operators {
+public class Operators {	
+
+
 public static void main(String[] args) {
-System.out.println(20 +30 - - 2 *( 3 + 4 ) + 2 + "Good" + ( 5 * 2 ) + ( 3 -1));
-}
-}
+	/*
+	int i=50;
+	i++;
+	i--;
+    boolean climate = false;
+    System.out.println(i <= 100);
+    System.out.println(i >= 10);
+  System.out.println(i == 100);
+     System.out.println(i < 70);
+     System.out.println(i > 50);
+    System.out.println(climate & cat());
+     if(i<100)
+     {
+	System.out.println("Rainning");
+    }
+
+	else if(i==50)
+	{
+		System.out.println("Not Raining");
+	}
+	else
+	{
+		System.out.println("Lenovo");
+	}
+
+    
+   switch (100) {
+	case 90:
+		System.out.println(90);
+		break;
+    case 20:
+	System.out.println(20);
+		break;
+    default:
+System.out.println("Default Value");
+		break;
+    case 1000:
+	System.out.println(1000);
+	break;
+    }
+    */
+   int value= 20;
+	
+	while(value>10) 
+	{
+		System.out.println("VALUE="+value);
+		value--;
+	}
+	
+	
+    
+    }
+  /*  public static boolean cat() 
+   {
+	return false;
+   }
+   */
+   }
